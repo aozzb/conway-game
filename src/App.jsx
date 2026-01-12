255c73dd62fea51243ad5ba738f3e50a076d400a
aozzb <brahmshiv05@gmail.com>
2026-01-12 14:13:05 +0530

    added lots of functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import createGrid from "./utils/createGrid";
 import Grid from "./components/Grid";
 import "./App.css";
@@ -32,9 +32,50 @@ const countAliveNeighbors = (grid, row, col) => {
   return count;
 };
 
+const createRandomGrid = (rows, cols) => {
+  const grid = [];
+
+  for (let i = 0; i < rows; i++) {
+    const row = [];
+    for (let j = 0; j < cols; j++) {
+      row.push(Math.random() > 0.7 ? 1 : 0);
+    }
+    grid.push(row);
+  }
+
+  return grid;
+};
+
 
 function App() {
   const [grid, setGrid] = useState(() => createGrid(ROWS, COLS));
+  const [isRunning, setIsRunning] = useState(false);
+
+  const intervalRef = useRef(null);
+
+  const startSimulation = () => {
+  if (intervalRef.current) return;
+
+  intervalRef.current = setInterval(() => {
+    nextGeneration();
+  }, 300);
+
+  setIsRunning(true);
+};
+
+useEffect(() => {
+  return () => {
+    clearInterval(intervalRef.current);
+  };
+}, []);
+
+
+const stopSimulation = () => {
+  clearInterval(intervalRef.current);
+  intervalRef.current = null;
+  setIsRunning(false);
+};
+
 
   const nextGeneration = () => {
   setGrid(prevGrid =>
@@ -65,18 +106,44 @@ function App() {
     );
   };
 
+  const resetGrid = () => {
+  stopSimulation(); // safety
+  setGrid(createGrid(ROWS, COLS));
+};
+
+const randomizeGrid = () => {
+  stopSimulation(); // important
+  setGrid(createRandomGrid(ROWS, COLS));
+};
+
+
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">
         Conway’s Game of Life
       </h1>
 
-      <button onClick={nextGeneration} style={{ marginBottom: "10px" }}>
-        Next Generation
-      </button>
+      <div style={{ marginBottom: "10px" }}>
+        {!isRunning ? (
+          <button onClick={startSimulation}>Start</button>
+        ) : (
+          <button onClick={stopSimulation}>Stop</button>
+        )}
 
+        <button onClick={nextGeneration} style={{ marginLeft: "10px" }}>
+          Next
+        </button>
+
+        <button onClick={resetGrid} style={{ marginLeft: "10px" }}>
+          Reset
+        </button>
+
+        <button onClick={randomizeGrid} style={{ marginLeft: "10px" }}>
+          Random
+        </button>
+      </div>
       <Grid grid={grid} toggleCell={toggleCell} />
-
     </div>
   );
 }
